9f13d4415caaebb04da827fbb6ca347bacb64c23
Augusto A. B. Branquinho <augustobranquinho@gmail.com>
2018-03-18 02:30:41 -0300

    Fix CI, for that is used a specific version of Event Sourcing Lib.

--- skip-the-dishes-command-resource/src/test/java/br/com/skip/the/dishes/command/resource/CommandResourceConfigTest.kt
+++ skip-the-dishes-command-resource/src/test/java/br/com/skip/the/dishes/command/resource/CommandResourceConfigTest.kt
@@ -1,5 +1,7 @@
 package br.com.skip.the.dishes.command.resource
 
+import br.com.skip.the.dishes.domain.customer.commands.CustomerCommandHandler
+import br.com.skip.the.dishes.domain.order.commands.OrderCommandHandler
 import br.com.skip.the.dishes.domain.product.Product
 import br.com.skip.the.dishes.domain.product.commands.CreateProductCommand
 import br.com.skip.the.dishes.domain.product.commands.ProductCommandHandler
@@ -22,8 +24,20 @@ class CommandResourceConfigTest {
         on { handle(any<CreateProductCommand>()) } doReturn product
     }
 
+    private val customerCommandHandler = mock<CustomerCommandHandler> { }
+
+    private val orderCommandHandler = mock<OrderCommandHandler> { }
+
     @Bean
     fun productCommandHandler(): ProductCommandHandler =
             productCommandHandler
+
+    @Bean
+    fun customerCommandHandler(): CustomerCommandHandler =
+            customerCommandHandler
+
+    @Bean
+    fun orderCommandHandler(): OrderCommandHandler =
+            orderCommandHandler
 
 }
