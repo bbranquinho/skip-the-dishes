a6b05f0fde308b5f7aac0ece24b38ff6e060a4bc
Augusto A. B. Branquinho <augustobranquinho@gmail.com>
2018-03-20 00:56:59 -0300

    Docker-compose for all components.

---
+++ wait-services.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+cd /home/application
+./wait-for-it.sh "postgres:5432" -- echo "postgres is up"
+./wait-for-it.sh "event-store:2113" -- echo "event-store 2113 is up"
+./wait-for-it.sh "event-store:1113" -- echo "event-store 1113 is up"
